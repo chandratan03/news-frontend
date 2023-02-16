98b8153ab6c4c76cf6c8d42c723f2fee6fd88272
chandratan03 <chandratan030501@gmail.com>
2023-02-16 15:53:18 +0700

    feat: register set auth

--- src/pages/register/UseRegister.jsx
+++ src/pages/register/UseRegister.jsx
@@ -1,32 +1,46 @@
-import React, { useState } from "react";
+import React, { useContext, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { register } from "../../apis/auth";
+import AuthContext from "../../contexts/AuthContext";
 
 export default function useRegister() {
     const navigate = useNavigate();
     const [errorMsg, setErrorMsg] = useState(undefined);
-
+    const { user, token, isAuth, setUser, setToken, setIsAuth } =
+        useContext(AuthContext);
     const onRegister = async (event) => {
         event.preventDefault();
 
         const firstName = event.target.first_name.value;
         const lastName = event.target.last_name.value;
-        
+
         const email = event.target.email.value;
         const password = event.target.password.value;
         const confirmPassword = event.target.password_confirmation.value;
 
-        let result = await register(firstName, lastName, email, password, confirmPassword);
+        let result = await register(
+            firstName,
+            lastName,
+            email,
+            password,
+            confirmPassword
+        );
         if (result.status === 201) {
             let data = result.data.data;
             sessionStorage.setItem("user", JSON.stringify(data.user));
             sessionStorage.setItem("token", data.token);
+            
+            setUser(sessionStorage.getItem("user"));
+            setToken(sessionStorage.getItem("token"));
+            setIsAuth(
+                sessionStorage.getItem("user") != null &&
+                    sessionStorage.getItem("token") != null
+            );
             navigate("/");
         } else {
             setErrorMsg(result.data.message);
         }
     };
-    
 
     return {
         errorMsg,
